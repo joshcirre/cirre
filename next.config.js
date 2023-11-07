353d58f193eb1a2c142d670c32bea76899fbce02
Josh Cirre <joshcirre@gmail.com>
2023-11-07 00:12:22 -0700

    add redirections

--- next.config.js
+++ next.config.js
@@ -12,6 +12,16 @@ module.exports = {
 				destination: 'https://cal.com/joshcirre',
 				permanent: true,
 			},
+            {
+                source: '/resume',
+                destination: 'https://drive.google.com/file/d/1fJs8VkWvH9fncp95sd51UyZMVK38F0mp/view?usp=sharing',
+                permanent: true,
+            },
+            {
+                source: '/resend',
+                destination: 'https://resend-uploadthing-attachments.vercel.app/',
+                permanent: true,
+            }
 		];
 	},
 };
